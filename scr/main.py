66abe333cb6007af905ef9acef5154fef159fa7a
Isaac Pawley <75295202+i-Saac-IV@users.noreply.github.com>
2023-11-11 11:11:28 +1300

    merging with isaac branch

--- scr/main.py
+++ scr/main.py
@@ -1,50 +1,120 @@
+"""
+
+
+
+"""
+
+import sys
+sys.path.insert(1, '../config/')
+from settings import *
+from util import *
 import pygame
-from sys import exit
+import random
+import math
+
+random.seed(SEED)
 
 pygame.init()
 
-#window setup:
-SCREEN_WIDTH, SCREEN_HEIGHT = 800, 400
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Template')
+width, height = 800, 800
+screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
 
-basicFont = pygame.font.SysFont(None, 30)
-#can change 'None' to path to .ttf file
-#text_surface = basicfont.render('whatever', True, WHITE)
+pygame.display.set_caption('Game of Survival')
 
-#Colours:
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-ORANGE = (200, 100, 0)
-BROWN = (150, 100, 50)
-YELLOW = (255, 255, 0)
-GREEN = (0, 255, 0)
-TEAL = (0, 255, 255)
-BLUE = (0, 0, 255)
-PURPLE = (255, 0, 255)
 
-#pygame.draw.line(surface, colour, (x, y), (x, y))
-#vem_surface = pygame.image.load('path/to/file.png').convert_alpha()
+basicFont = pygame.font.SysFont(None, 15)
+# can change 'None' to path to .ttf file
+text_surface = basicFont.render('whatever', True, WHITE)
 
-#main loop:
+# pygame.draw.line(surface, colour, (x, y), (x, y))
+# vem_surface = pygame.image.load('path/to/file.png').convert_alpha()
+
+
+""" ----------------------------------------- Variables ----------------------------------------- """
+
+board_radius = 5
+board_size = calc_num_tiles(board_radius)
+board = [[index_to_coords(i), random.randint(0,1)] for i in range(board_size)]
+tileSize = ((math.sqrt(3) * ((min(height, width) - 60) / math.sqrt(3))) / (board_radius * 3 - 1))
+print(f"Radius: {board_radius} Tiles: {board_size}")
+
+
+""" ----------------------------------------- Custom Funtions ----------------------------------------- """
+
+# draw a hexagon
+def draw_hexagon(surface, x, y, radius, pointUp, colour, jkl):
+
+    points = []
+    for i in range(6):
+        angle = i * (2 * math.pi / 6)
+        if pointUp == True:
+            angle -= (math.pi / 6)
+        xi = x + radius * math.cos(angle)
+        yi = y + radius * math.sin(angle)
+        points.append((xi, yi))
+    pygame.draw.polygon(surface, colour, points)
+    pygame.draw.polygon(surface, PURPLE, points, 3)
+    text_surface = basicFont.render(str(jkl), True, PURPLE)
+    # surface.blit(text_surface, (x, y))
+
+# draw the entire board
+def draw_board():
+    vert = (3 / 2) * tileSize
+    horiz = math.sqrt(3) * tileSize
+    for i in range(board_size):
+        jkl = (board[i][0][0], board[i][0][1], board[i][0][2])
+        match board[i][1]:
+            case 0: # dead
+                draw_hexagon(screen, (width / 2) + (horiz * board[i][0][0]) + (horiz * board[i][0][1] / 2), (height / 2) + (vert * board[i][0][1]), tileSize, True, GREY, jkl)
+            case 1: # alive
+                draw_hexagon(screen, (width / 2) + (horiz * board[i][0][0]) + (horiz * board[i][0][1] / 2), (height / 2) + (vert * board[i][0][1]), tileSize, True, WHITE, jkl)
+            case 2: # fungi
+                draw_hexagon(screen, (width / 2) + (horiz * board[i][0][0]) + (horiz * board[i][0][1] / 2), (height / 2) + (vert * board[i][0][1]), tileSize, True, YELLOW, jkl)
+            case 3: # virus
+                draw_hexagon(screen, (width / 2) + (horiz * board[i][0][0]) + (horiz * board[i][0][1] / 2), (height / 2) + (vert * board[i][0][1]), tileSize, True, GREEN, jkl)
+
+
+def check_neighbours(coords):
+    i = 0
+
+""" ----------------------------------------- Main Loop ----------------------------------------- """
+
 frames_clock = pygame.time.Clock()
 while True:
+
+    # stops the loop, for testing funtions quickly
+
     #inputs:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            exit()
-        #if event.type == pygame.KEYDOWN:
-        if event.type == pygame.KEYUP:
+            sys.exit()
+        if event.type == pygame.VIDEORESIZE:
+            width, height = event.w, event.h
+            screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
+            tileSize = ((math.sqrt(3) * ((min(height, width) - 60) / math.sqrt(3))) / (board_radius * 3 - 1))
+        if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
-                exit()
-        #if event.type == pygame.MOUSEBUTTONUP:
+                sys.exit()
+        if event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                print(pygame.mouse.get_pos())
+                print(xy_to_jkl(pygame.mouse.get_pos(), width, height, tileSize))
+                if board[0][1] == 0:
+                    pass
+                else:
+                    pass
+        # if event.type == pygame.KEYUP:
     
-    #draw everything:
+    # draw everything:
     screen.fill(BLACK)
-    #screen.blit(surface, (x,y))
+    draw_board()
+    #for i in range(board_size):
+    #    board[i] = ((board[i][0]), random.randint(0,3))
+    # screen.blit(text_surface, (0,0))
+    # screen.blit(surface, (x,y))
     
     pygame.display.update()
     frames_clock.tick(60)
+
